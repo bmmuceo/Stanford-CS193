f7ad1ae835901ed5d0e4d41219d0363082daa34e
bmmuceo <bmmuceo@gmail.com>
2025-04-08 05:51:27 -0400

    Initial Commit

---
+++ Stanford CS193/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Stanford CS193
+//
+//  Created by RyanA on 4/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Stanford CS193/Stanford_CS193App.swift
@@ -0,0 +1,17 @@
+//
+//  Stanford_CS193App.swift
+//  Stanford CS193
+//
+//  Created by RyanA on 4/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Stanford_CS193App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
