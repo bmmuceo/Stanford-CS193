c1c9f7e1b3e37402b5775ca1c438528bac48a03c
bmmuceo <bmmuceo@gmail.com>
2025-04-22 23:06:02 -0400

    L5

--- Stanford CS193/EmojiMemoryGameView.swift
+++ Stanford CS193/EmojiMemoryGameView.swift
@@ -7,23 +7,21 @@
 
 import SwiftUI
 
-
 struct EmojiMemoryGameView: View {
     var body: some View {
-        MultipleCards (viewModel: EmojiMemoryGame.init())
+        MultipleCards(viewModel: EmojiMemoryGame())
     }
 }
 
-
 struct MultipleCards: View {
-    
-    var viewModel: EmojiMemoryGame = EmojiMemoryGame()
-    
-    
+
+    @ObservedObject var viewModel: EmojiMemoryGame
+
     var body: some View {
         VStack {
             ScrollView {
                 cards
+                    .animation(.default, value: viewModel.cards)
             }
             Button("Shuffle") {
                 viewModel.shuffle()
@@ -31,100 +29,77 @@ struct MultipleCards: View {
         }
         .padding()
     }
-    
+
     var cards: some View {
-        LazyVGrid(columns:  [GridItem(.adaptive(minimum: 75), spacing: 0)], spacing: 0) {
-            ForEach(viewModel.cards.indices, id: \.self) { index in
-                CardView(viewModel.cards[index])
-                    .aspectRatio(2/3, contentMode: .fit)
+        LazyVGrid(
+            columns: [GridItem(.adaptive(minimum: 75), spacing: 0)],
+            spacing: 0
+        ) {
+            ForEach(viewModel.cards) { card in
+                CardView(card)
+                    .aspectRatio(2 / 3, contentMode: .fit)
                     .padding(4)
+                    .onTapGesture {
+                        viewModel.choose(card)
+                    }
+
             }
         }
         .padding()
-        .foregroundStyle(Gradient(colors: [.vibess,.wwww,.nott,.wwww,.vibess,.nott,.wwww,.nott,.wwww]))
+        .foregroundStyle(
+            Gradient(colors: [
+                .vibess, .wwww, .nott, .wwww, .vibess, .nott, .wwww, .nott,
+                .wwww,
+            ])
+        )
     }
 }
-/* var cardCountAdjusters: some View {
- HStack {
- Spacer()
- cardRemover
- Spacer()
- cardAdder
- Spacer()
- }
- .font(.largeTitle.bold())
- .imageScale(.large)
- .blendMode(.hardLight)
- .foregroundStyle(Gradient(colors: [.vibess,.nott,.wwww,.vibess,]))
- }
- 
- func cardCountAdjuster(by offset: Int,symbol : String) -> some View {
- Button(action: {
- cardCount += offset
- }, label: {
- Image(systemName: symbol)
- })
- .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
- }
- 
- var cardRemover: some View {
- cardCountAdjuster(by: -1, symbol: "minus.square")
- }
- var cardAdder: some View {
- cardCountAdjuster(by: +1, symbol: "plus.square")
- 
- }
- }*/
-
 
 struct CardView: View {
     let card: MemoryGame<String>.Card
-    
+
     init(_ card: MemoryGame<String>.Card) {
         self.card = card
     }
-    
+
     var body: some View {
+
         ZStack {
-            
             let rRect = RoundedRectangle(cornerRadius: 12)
-            
+
             if card.isFaceUp {
-                Group {
-                    rRect
-                        .foregroundStyle(Gradient(colors: [.nott,.wwww,.vibess,]))
-                    rRect
-                        .strokeBorder(.red,lineWidth: 3)
-                        .shadow(color: .red, radius: 7)
-                        .brightness(Float64(0.8))
-                    Text(card.content)
-                        .font(.system(size: 200))
-                        .minimumScaleFactor(0.01)
-                        .aspectRatio(1, contentMode: .fit)
-                        .shadow(color: .green, radius: 0.8)
-                        .brightness(0.3)
-                }
+                rRect
+                rRect
+                    .strokeBorder(.red, lineWidth: 3)
+                    .shadow(color: .red, radius: 7)
+                    .brightness(Float64(0.8))
+                Text(card.content)
+                    .font(.system(size: 200))
+                    .minimumScaleFactor(0.01)
+                    .aspectRatio(1, contentMode: .fit)
+                    .shadow(color: .green, radius: 0.8)
+                    .brightness(0.3)
+            } else if card.isMatched {
+                rRect
+                    .opacity(0)
+
             } else {
                 rRect
-                    .fill(.opacity(0.4))
+                    .fill(.opacity(0.5))
                     .strokeBorder(lineWidth: 5)
                     .shadow(color: .white, radius: 8)
                 Text("⛄︎")
                     .font(.system(size: 200))
                     .minimumScaleFactor(0.01)
-                    .aspectRatio(1, contentMode:.fit)
-                    .brightness(0.9)
+                    .aspectRatio(1, contentMode: .fit)
                     .shadow(color: .pink, radius: 5.0)
-                
+                    .brightness(1.4)
             }
         }
     }
 }
 
-
-
 #Preview {
     EmojiMemoryGameView()
         .preferredColorScheme(.dark)
 }
-
